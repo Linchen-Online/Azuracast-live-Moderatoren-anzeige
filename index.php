8674fecd7ab4a787cba883660c92a05c24f36990
Linchen-Online <55755582+Linchen-Online@users.noreply.github.com>
2019-09-24 20:21:43 +0200

    Code zur Anzeige des Azuracast Live Moderators

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$station = "xx"; //ID Der Station bei Azuracast
+$json_station = file_get_contents("https://domain-zu-azuracast.endung/api/nowplaying/".$station);
+$obj_station = json_decode($json_station);
+$live = $obj_station->live->streamer_name;
+$pls = $obj_station->now_playing->playlist;
+?>
+
+<img src="//link-zur-den-bildern-die-angezeigt-werden.sollen/<?php if(empty($live)) {echo $pls;}else{echo $live;}
+;?>.png" alt="<?php if(empty($live)) {echo $pls;}else{echo $live;}
+?>" style="width:350px; height:auto;"><br>
+<a href="https://linchen.online" target="_blank"> Project-Radio - Linchen </a>
